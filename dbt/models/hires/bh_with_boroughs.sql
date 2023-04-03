d7fdba87299acabe4c952c63995686c495c99d40
Oleg Volchenko <oleg.volchenko@ingka.ikea.com>
2023-04-03 19:43:01 +0200

    fix fact with boroughs

--- dbt/models/hires/bh_with_boroughs.sql
+++ dbt/models/hires/bh_with_boroughs.sql
@@ -4,9 +4,9 @@ SELECT
     fact.end_date,
     fact.duration,
     fact.startstation_name,
-    start_boroughs.station_name AS startstation_borough,
+    start_boroughs.borough AS startstation_borough,
     fact.endstation_name,
-    start_boroughs.station_name AS endstation_borough
+    start_boroughs.borough AS endstation_borough
 FROM {{ ref('bh_fact') }} AS fact
-LEFT JOIN {{ ref('dim_boroughs') }} AS start_boroughs ON fact.startstation_name = start_boroughs.station_name
-LEFT JOIN {{ ref('dim_boroughs') }} AS end_boroughs ON fact.endstation_name = end_boroughs.station_name
+LEFT JOIN {{ ref('dim_boroughs') }} AS start_boroughs ON REPLACE(fact.startstation_name,',','') = start_boroughs.station_name
+LEFT JOIN {{ ref('dim_boroughs') }} AS end_boroughs ON REPLACE(fact.startstation_name,',','')  = end_boroughs.station_name
