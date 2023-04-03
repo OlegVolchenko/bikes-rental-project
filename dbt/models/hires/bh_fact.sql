643f7cff02fd01d9a0ee880db818eab9145b7c2b
Oleg Volchenko <oleg.volchenko@ingka.ikea.com>
2023-04-03 16:57:49 +0200

    fix fact table

--- dbt/models/hires/bh_fact.sql
+++ dbt/models/hires/bh_fact.sql
@@ -1,10 +1,20 @@
 {{ config(materialized='view') }}
 
+with hires AS (
 SELECT
   rental_id,
   CAST(start_date AS DATETIME) AS start_date,
-  CAST(start_date AS DATETIME) AS start_date,
-  DATETIME_DIFF(end_date, start_date, MINUTE) AS duration,
+  CAST(end_date AS DATETIME) AS end_date,
   startstation_name,
   endstation_name
 FROM {{ source('staging','trips_export') }}
+
+)
+SELECT
+  rental_id,
+  start_date,
+  end_date,
+  DATETIME_DIFF(end_date, start_date, MINUTE) AS duration,
+  startstation_name,
+  endstation_name
+FROM hires
