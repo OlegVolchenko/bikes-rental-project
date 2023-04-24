6213456837a9b6e89a478dd227a9bb712554eb48
Oleg Volchenko <oleg.volchenko@ingka.ikea.com>
2023-04-24 11:20:34 +0200

    add credentials block

--- prefect/blocks/gcp_prefect_deployments_credentials.py
+++ prefect/blocks/gcp_prefect_deployments_credentials.py
@@ -1,14 +1,15 @@
 import os
 
-from prefect.filesystems import GCS
+from prefect_gcp import GcpCredentials
 
 with open(os.environ['PREFECT_SA']) as f:
     service_account = f.read()
 
 project = os.environ['PROJECT']
-block = GCS(
-    bucket_path=f"prefect-deployments_{project}/dev/",
+
+credentials = GcpCredentials(
     service_account_info=service_account,
     project=project
 )
-block.save("dev", overwrite=True)
+
+credentials.save("default-credentials", overwrite=True)
