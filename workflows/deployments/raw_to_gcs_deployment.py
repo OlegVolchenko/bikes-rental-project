f684789b2140526b96b17afbe7ceb15a31334ae7
Oleg Volchenko <oleg.volchenko@ingka.ikea.com>
2023-04-26 18:48:51 +0200

    update gcs deployment to use github block

--- workflows/deployments/raw_to_gcs_deployment.py
+++ workflows/deployments/raw_to_gcs_deployment.py
@@ -1,14 +1,15 @@
 from prefect.deployments import Deployment
-from prefect.filesystems import GCS
+from prefect.filesystems import GitHub
 
 from workflows.flows.extract_pipeline.raw_to_gcs import load_to_datalake
 
-gcs_block = GCS.load("deployments")
+github_block = GitHub.load("github")
 deployment = Deployment.build_from_flow(
     flow=load_to_datalake,
     name="load_to_datalake_deployment",
-    storage=gcs_block,
+    storage=github_block,
     version=1,
     work_queue_name="main")
+
 if __name__ == '__main__':
     deployment.apply()
