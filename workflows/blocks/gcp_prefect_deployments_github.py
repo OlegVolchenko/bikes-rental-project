41b02d3e7f3a5bdc306728ea26e68fe897f2b296
Oleg Volchenko <oleg.volchenko@ingka.ikea.com>
2023-05-02 14:26:57 +0200

    fix blocks deployment script

--- workflows/blocks/gcp_prefect_deployments_github.py
+++ workflows/blocks/gcp_prefect_deployments_github.py
@@ -3,4 +3,4 @@ from prefect.filesystems import GitHub
 block = GitHub(
     repository="https://github.com/OlegVolchenko/bikes-rental-project.git",
 )
-block.save("github")
+block.save("github", overwrite=True)
--- workflows/prefect_deployment.sh
+++ workflows/prefect_deployment.sh
@@ -1,10 +1,11 @@
+#!/bin/bash
 #blocks
 python blocks/gcp_prefect_deployments_credentials.py
-python blocks/gcp_prefect_deployments_github.py
-python blocks/gcp_prefect_deployments_storage.py
-python blocks/gcp_prefect_deployment_dbt_profile.py
+python workflows/blocks/gcp_prefect_deployments_github.py
+python workflows/blocks/gcs_prefect_deployments_storage.py
+python workflows/blocks/gcs_prefect_deployments_dbt_profile.py
 #flows
-python deployments/gcs_to_bq_deployment.py
-python deployments/geospatial_deployment.py
-python deployments/raw_to_gcs_deployment.py
-python deployments/dbt_deployment.py
+python workflows/deployments/gcs_to_bq_deployment.py
+python workflows/deployments/geospatial_deployment.py
+python workflows/deployments/raw_to_gcs_deployment.py
+python workflows/deployments/dbt_deployment.py
